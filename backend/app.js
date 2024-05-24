b42304875c0dd774be0acf775da540bd76ed8ac9
chinmaygs <164546850+chinmaygs@users.noreply.github.com>
2024-05-24 10:18:12 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from"cors";
+import cookieParser from "cookie-parser";
+import postRoute from"./routes/post.route.js";
+import authRoute from"./routes/auth.route.js";
+import testRoute from"./routes/test.route.js";
+import userRoute from"./routes/user.route.js";
+import chatRoute from"./routes/chat.route.js";
+import messageRoute from"./routes/message.route.js";
+
+const app=express();
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+  });
+
+
+app.use(cors({origin:process.env.CLIENT_URL, credentials: true}))
+app.use(cookieParser())
+app.use(express.json())
+
+app.use('/api/auth',authRoute)
+app.use('/api/users',userRoute)
+app.use('/api/posts',postRoute)
+app.use('/api/test',testRoute)
+app.use('/api/chats',chatRoute)
+app.use('/api/messages',messageRoute)
+
+
+app.listen(8000,()=>{
+    console.log("Server is running!")
+})
