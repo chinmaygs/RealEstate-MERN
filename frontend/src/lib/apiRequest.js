401c47385bd6bfd25cdb22da19a364aa55132ab7
chinmaygs <164546850+chinmaygs@users.noreply.github.com>
2024-05-27 14:42:16 +0530

    Update apiRequest.js

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+axios.defaults.withCredentials = true;
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:8000/api",
+    baseURL:"https://realestate-mern-nt9u.onrender.com",
     withCredentials: true,
 });
 
